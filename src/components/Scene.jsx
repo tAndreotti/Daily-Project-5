e808b97326c88d9f87c17247f9c20b6583173bfa
Thiago Andreotti <dmpk02@gmail.com>
2024-03-01 19:56:17 -0300

    5st Project v1.01

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,17 +1,32 @@
+// "use client";
+// import { Canvas } from "@react-three/fiber";
+// import Model from "./Model";
+// import { Environment, CameraControls } from "@react-three/drei";
+
+// export default function Scene() {
+//   return (
+//     <Canvas
+//       orthographic
+//       style={{ background: "black" }}
+//       camera={{ position: [0, 0, 1], zoom: 800 }}
+//     >
+//       <directionalLight intensity={3} position={[0, 0.1, 1]} />
+//       <Model />
+//       <Environment preset="city" />
+//     </Canvas>
+//   );
+// }
+
 "use client";
 import { Canvas } from "@react-three/fiber";
 import Model from "./Model";
-import { Environment, CameraControls } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 
-export default function Scene() {
+export default function Index() {
   return (
-    <Canvas
-      orthographic
-      style={{ background: "black" }}
-      camera={{ position: [0, 0, 1], zoom: 800 }}
-    >
-      <directionalLight intensity={3} position={[0, 0.1, 1]} />
+    <Canvas style={{ background: "#000000" }}>
       <Model />
+      <directionalLight intensity={2} position={[0, 2, 3]} />
       <Environment preset="city" />
     </Canvas>
   );
